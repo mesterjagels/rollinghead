3d8e63832717edbe4c97e94f9e312cd6ae7faefb
Jasper van't Veen <jvanveen@gmail.com>
2016-01-30 17:23:59 +0100

    no message

---
+++ Head of Apocalyptica/Assets/SpikeAnimation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeAnimation : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+        GetComponent<Animator>().Play("SpikeDeath");
+    }
+}
