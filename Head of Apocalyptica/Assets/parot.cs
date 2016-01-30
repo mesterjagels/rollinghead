af7c639a3379bcd80fb0f0e71bd5569f4cecfe4a
Jasper van't Veen <jvanveen@gmail.com>
2016-01-30 23:08:35 +0100

    no message

---
+++ Head of Apocalyptica/Assets/parot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parot : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+	// Update is called once per frame
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+	void Update () {
+	    if(transform.position.x < -10f)
+        {
+            transform.position = new Vector2(12f, transform.position.y);
+        }
+	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        collider.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * 2, collider.GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
---
+++ Head of Apocalyptica/Assets/parotleft.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parotleft : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+    // Update is called once per frame
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+    void Update()
+    {
+        if (transform.position.x > 12f)
+        {
+            transform.position = new Vector2(-12f, transform.position.y);
+        }
+    }
+}
