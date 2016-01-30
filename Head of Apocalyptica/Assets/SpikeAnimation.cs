e755147b457302fc6c74084b0cbcb4ac26144ac9
Jasper van't Veen <jvanveen@gmail.com>
2016-01-30 20:28:29 +0100

    no message

--- Head of Apocalyptica/Assets/SpikeAnimation.cs
+++ Head of Apocalyptica/Assets/SpikeAnimation.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
+
 public class SpikeAnimation : MonoBehaviour {
+    public GameObject text;
+    private Canvas canvas;
 
 	void OnTriggerEnter2D(Collider2D collider)
     {
         GetComponent<Animator>().Play("SpikeDeath");
         GetComponent<AudioSource>().Play();
+        text.GetComponent<Text>().text = "Press 'R' to Restart";
     }
 }
