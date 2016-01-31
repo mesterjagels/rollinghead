a85a12d9d6acfe6cd1cf3963189f84ba1153a732
Jasper van't Veen <jvanveen@gmail.com>
2016-01-31 11:49:31 +0100

    No errors!!!

--- Head of Apocalyptica/Assets/Scripts/LevelHandler.cs
+++ Head of Apocalyptica/Assets/Scripts/LevelHandler.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LevelHandler : MonoBehaviour {
@@ -6,20 +7,37 @@ public class LevelHandler : MonoBehaviour {
     int levelIndex = 0;
     private float playerPosX;
     public GameObject win;
+    public bool levelCompleted;
+    public bool playerDead;
+    public Text text;
+    private Canvas canvas;
 
-  
+    void Awake()
+    {
+        levelCompleted = false;
+        playerDead = false;
+        canvas = FindObjectOfType<Canvas>();
+    }
+
     void Update()
     {
-        playerPosX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
-        if (Input.GetKey(KeyCode.Space))
-        {
-            levelIndex = levelIndex + 1;
-            Application.LoadLevel(levelIndex);
+        if (levelCompleted) {
+            canvas.enabled = true;
+               
+            if (Input.GetKey(KeyCode.Space))
+            {
+                print("Change level mofo!");
+                levelIndex = levelIndex + 1;
+                Application.LoadLevel(levelIndex);
+            }
+        
         }
     }
+
 	void OnTriggerEnter2D(Collider2D collider)
     {
+        levelCompleted = true;
         playerPosX = GameObject.FindGameObjectWithTag("Player").transform.position.x;
-        Instantiate(win, new Vector3(playerPosX, transform.position.y + 2, transform.position.z), Quaternion.identity);
+        Instantiate(win, new Vector3(playerPosX, transform.position.y + 10.5f, transform.position.z), Quaternion.identity);
     }
 }
